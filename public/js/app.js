0f78bfd6356896edf3678852fcce24367962a43e
sbrycebarker <sbrycesti@gmail.com>
2016-07-30 06:00:58 -0600

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -2,28 +2,28 @@ angular.module("ecommerce", ['ui.router']).config(function ($stateProvider, $url
         $stateProvider
             .state('home',{
                 url:'/',
-                templateUrl: "../html/index.html"
+                templateUrl: "../index.html"
             })
-            .state('turbos',{
+            .state('Packages',{
+                url:'/turbos',
+                parent:'home',
+                templateUrl: "../html/packages.html"
+            })
+            .state('Performance',{
                 url:'/turbos',
                 parent:'home',
                 templateUrl: "../html/turbos.html"
             })
-            .state('contact',{
-                url:'/contact',
-                parent:'home',
-                templateUrl: "../html/contact.html"
-            })
-            .state('packages',{
-                url:'/packages',
-                templateUrl: "../html/packages.html",
-                controller: 'packagesCtrl'
+            .state('cosmetics',{
+                url:'/cosmetics',
+                templateUrl: "../html/cosmetics.html",
+                controller: 'cosmeticsCtrl'
 
             })
-            .state('downpipes',{
-                url:'/booked/:id',
-                templateUrl: "../html/downpipes.html",
-                controller: 'downpipesCtrl'
+            .state('contact',{
+                url:'/contact/:id',
+                templateUrl: "../html/contact.html",
+                controller: 'contacCtrl'
 
             })
             .state('intakes',{
