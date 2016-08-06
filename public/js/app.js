e77005de7448e687208c10dd21565f6f07c8b064
sbrycebarker <sbrycesti@gmail.com>
2016-08-06 15:28:37 -0600

    sat6

--- public/js/app.js
+++ public/js/app.js
@@ -7,48 +7,42 @@ angular.module("ecommerce", ['ui.router']).config(function ($stateProvider, $url
             .state('turbos',
             {
                 url:'/turbos',
-                parent:'home',
-                templateUrl: "../html/turbos.html"
+                templateUrl: "./html/turbos.html"
             })
             .state('exhaust',{
                 url:'/exhaust',
-                parent:'home',
-                templateUrl: "../html/exhaust.html"
+                templateUrl: "./html/exhaust.html"
             })
             .state('intercoolers',{
                 url:'/intercoolers',
-                templateUrl: "../html/intercoolers.html"
+                templateUrl: "./html/intercoolers.html"
 
             })
             .state('clothing',{
                 url:'/clothing',
-                templateUrl: "../html/clothing.html"
+                templateUrl: "./html/clothing.html"
 
             })
             .state('keychains',{
                 url:'/keychains',
-                parent:'home',
-                templateUrl: "../html/keychains.html"
+                templateUrl: "./html/keychains.html"
             })
-            .state('steering-wheels',{
-                url:'/steering-wheels',
-                parent:'home',
-                templateUrl: "../html/steeringwheels.html"
+            .state('steeringwheels',{
+                url:'/steeringwheels',
+                templateUrl: "./html/steeringwheels.html"
             })
             .state('seats',{
                 url:'/seats',
-                parent:'home',
-                templateUrl: "../html/seats.html"
+                templateUrl: "./html/seats.html"
             })
             .state('shiftknobs',{
                 url:'/shiftknobs',
-                parent:'home',
-                templateUrl: "../html/shiftknobs.html"
+                templateUrl: "./html/shiftknobs.html"
             })
             .state('contact',{
                 url:'/contact/:id',
-                templateUrl: "../html/contact.html",
-                controller: 'contacCtrl'
+                templateUrl: "./html/contact.html",
+                // controller: 'contacCtrl'
 
             })
 
